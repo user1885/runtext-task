49fb3ca28ef99d3ed3ef4fdc4d8986879b6e817b
user1885 <item0time@gmail.com>
2024-05-19 14:36:54 +0300

    Добавил поддержку PostgreSQL

--- runtext_site/settings.py
+++ runtext_site/settings.py
@@ -81,9 +81,17 @@ WSGI_APPLICATION = 'runtext_site.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'runtext',
+        'USER': 'postgress',
+        'PASSWORD': '2323',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
