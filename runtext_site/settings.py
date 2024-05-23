2b5651eee72c72a85d597b5d398a98d912e7e264
mark <99057387+user1885@users.noreply.github.com>
2024-05-23 20:09:41 +0300

    Update settings.py

--- runtext_site/settings.py
+++ runtext_site/settings.py
@@ -34,7 +34,7 @@ DEBUG = True
 # ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = []
-CRSF_TRUSTED_ORIGINS.append(os.getenv('CSRF_TRUSTED_ORIGINS'))
+CSRF_TRUSTED_ORIGINS.append(os.getenv('CSRF_TRUSTED_ORIGINS'))
 
 # Application definition
 
